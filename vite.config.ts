e4955489067acfe75a2e2bdbf1933251950ddcdc
TakanariShimbo <takanari.shimbo@gmail.com>
2025-09-03 23:53:26 +0900

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const [owner, repo] = (process.env.GITHUB_REPOSITORY || ":").split("/");
+const isActions = !!process.env.GITHUB_ACTIONS;
+const isUserSite = repo && owner && repo.toLowerCase() === `${owner.toLowerCase()}.github.io`;
+const base = isActions ? (isUserSite ? "/" : `/${repo}/`) : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base,
+});
